41143bffe72bdcc2739bfe10534d111b9b94a751
Mayank Kumar <53262974+MayankKumar10@users.noreply.github.com>
2022-04-17 18:15:43 +0530

    adding changes to vision kart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -100,7 +100,7 @@ export function Header() {
             >
               <a
                 className="headerAnchorTag flex-column-center"
-                href="./Login"
+                href="login"
                 target="iframe-main-container"
               >
                 <span className="button-inner-txt">
